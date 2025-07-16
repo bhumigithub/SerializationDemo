dd5576abd5878a590a5ef681dde10be9bb2f74f9
bhumi <bhumitungar@gmail.com>
2025-07-16 14:59:07 +0530

    2nd commit on emp file

--- src/com/demo/Employee.java
+++ src/com/demo/Employee.java
@@ -5,14 +5,13 @@ import java.io.Serializable;
 public class Employee implements Serializable{
 
 	private static final long serialVersionUID = 8670234833920069280L;
+	
 	int id;
 	String name;
 	
 	public Employee(int id, String name) {
-		
 		this.id=id;
 		this.name=name;
-		
 	}
 
 
